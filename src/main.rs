34ac4925e204d9dffb5e3dc6b1b54976cfda06a1
danny <daniel.noest@disneystreaming.com>
2021-07-17 23:20:17 -0400

    Load file and run it

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+use hex;
+use std::fs;
+use structopt::StructOpt;
+
+
 mod cpu;
 mod opcodes;
 
-fn main() {
-    let mut cpu = cpu::CPU::new();
 
-    let program : [u8; 6] = [
-        0x80, 0x14,
-        0x80, 0x24,
-        0x80, 0x34];
+#[derive(StructOpt)]
+struct Cli {
+    filename: String
+}
+
+fn read_rom_hexadecimal<'a>(filename: &str) -> Vec<u8> {
+    let contents = fs::read_to_string(filename).unwrap();
+    let filtered: String = contents.chars().filter(|c| c.is_digit(16)).collect();
+    hex::decode(filtered).unwrap()
+}
+
+fn main() {
+
+    let args = Cli::from_args();
+    let program = read_rom_hexadecimal(&args.filename);
+    println! ("{:#04X?}", program);
+
+    let mut cpu = cpu::CPU::new();
     cpu.load(&program); 
     cpu.run();
-
-
 }
